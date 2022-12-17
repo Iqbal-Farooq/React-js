8a8d05ae69e18d31b887adc50f278d3e78e7e445
iqbal-Farooq <akashfarooq222@gmail.com>
2022-12-17 18:30:22 +0530

    USING useState hook to change the state of the components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,13 +1,28 @@
 import './Expense.css'
-import React from 'react';
+import React ,{useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Wrapper from '../UI/Wrapper';
 const ExpenseItem=(props)=>{
+   let [amount,setAmount]= useState(props.amount)
+    let [title,setTitle]= useState(props.title)
  const del=()=>{
 
    let child=document.getElementById(props.id);
    child.remove();
+   console.log("DELETED SUCESSFULLY")
+
+  
+ }
+
+let update=()=>{
+  setAmount('$-100')
+   setTitle('Updated!');
+
+   
+   console.log(amount)
+   console.log(title)
+   
 
   
  }
@@ -18,8 +33,9 @@ const ExpenseItem=(props)=>{
         <Wrapper id={props.id}>
            <h3 className='item' id='i'><mark>EXPENSE ITEMS</mark> </h3>
               <ExpenseDate  date={props.date} />
-                <ExpenseDetails id={props.id} amount={props.amount} title={props.title} Location={props.Location}/>
-                <div><button onClick={del}>Del</button></div>
+                <ExpenseDetails id={props.id} amount={amount} title={title} Location={props.Location}/>
+                <div><button onClick={del}>Delete</button> <button onClick={update}>update</button></div>
+                
                
              
  

14390854811c9cb96ce7da14c6026620e515454b
iqbal-Farooq <akashfarooq222@gmail.com>
2022-12-18 10:44:58 +0530

    using useState hook for input values

--- src/components/Expenses/Input.js
+++ src/components/Expenses/Input.js
@@ -1,21 +1,46 @@
+import React,{useState} from "react";
 function Input(){
-    const print=(event)=>{
-        event.preventDefault();
+    const[enteredTitle,setEnteredTitle]=useState('')
+    const[enteredAmount,setEnteredAmount]=useState('')
+    const [entereDate,setEnteredDate]=useState('');
+
+     const titleChanged=(e)=>{
+        console.log(e.target.value);
+        setEnteredTitle(e.target.value);
        
+       }
 
-        let cat=document.getElementById('type').value;
-        let amount=document.getElementById('amount').value;
-        let date=document.getElementById('date').value;
-        console.log(cat+" "+amount+" "+date);
+            const amountChanged=(e)=>{
+                console.log(e.target.value);
+                setEnteredAmount(e.target.value)
 
-    }
-    return(
-        <form>
-        <input type='text' id="type" placeholder="expanse-name"/><br /><br />
-         <input type='number' id="amount" placeholder="expense-amount"/><br /><br />
-         <input type='date'  id="date" /> <br /><br />
-         <button onClick={print}>Submit</button><br /><br />
-         </form>
-    )
-}
-export default Input;
+            }
+
+                        const dateChanged=(e)=>{
+                        console.log(e.target.value);
+                        setEnteredDate(e.target.value)
+
+                    }
+                                                
+                                const print=(event)=>{
+                                    event.preventDefault();
+                                
+
+                                    let cat=document.getElementById('type').value;
+                                    let amount=document.getElementById('amount').value;
+                                    let date=document.getElementById('date').value;
+                                    console.log(cat+" "+amount+" "+date);
+                                
+
+                                }
+
+                return(
+                    <form>
+                    <input type='text' id="type" placeholder="expanse-name" onChange={titleChanged}/><br /><br />
+                    <input type='number' id="amount" placeholder="expense-amount" onChange={amountChanged}/><br /><br />
+                    <input type='date'  id="date" onChange={dateChanged}/> <br /><br />
+                    <button onClick={print}>Submit</button><br /><br />
+                    </form>
+                )
+            }
+            export default Input;
